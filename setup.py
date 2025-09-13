90989745565f0e8c2654527f77082831c4de1372
STiFLeR7 <hillaniljppatel@gmail.com>
2025-09-13 14:43:14 +0530

    v2.11.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="imgshape",
-    version="2.1.3",
+    version="2.1.4",
     description="Smart image shape, dataset analysis, preprocessing & augmentation recommendations for ML workflows",
     long_description=long_description,
     long_description_content_type="text/markdown",
